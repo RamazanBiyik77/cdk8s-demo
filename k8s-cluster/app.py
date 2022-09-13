b7fc5201ef2cd97895b354a0fff875773136894f
Ramazan BIYIK <ramazanbiyik@Ramazans-MacBook-Pro.local>
2022-09-13 17:17:21 +0300

    Initial commit

---
+++ k8s-cluster/app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from distutils import core
+import os
+
+import aws_cdk as cdk
+from aws_cdk import aws_iam as iam
+from aws_cdk import aws_eks as eks
+from aws_cdk import aws_ec2 as ec2
+from aws_cdk import core
+from k8s_cluster.k8s_cluster_stack import ClusterStack
+
+app = core.App()
+ClusterStack(app, "K8SClusterStack",
+    # If you don't specify 'env', this stack will be environment-agnostic.
+    # Account/Region-dependent features and context lookups will not work,
+    # but a single synthesized template can be deployed anywhere.
+
+    # Uncomment the next line to specialize this stack for the AWS Account
+    # and Region that are implied by the current CLI configuration.
+
+    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+
+    # Uncomment the next line if you know exactly what Account and Region you
+    # want to deploy the stack to. */
+
+    env=core.Environment(account='132248825767', region='eu-west-3'),
+
+    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+    )
+
+app.synth()
